a95f32ca943efbab55656b3cc9483891e6eb58ee
v341196137 <38384400+v341196137@users.noreply.github.com>
2020-10-24 15:55:07 -0400

    Create Math.py

---
+++ GameClasses/Math.py
@@ -0,0 +1,37 @@
+# Math.py
+# 2020/10/24
+# Idk bro, procrastination
+# Just gives math questions???
+import random
+class Math():
+    def __init__(self):
+        self.n1 = random.randrange(100)
+        self.n2 = random.randrange(100)
+        self.operator = None
+        self.answer = None
+        temp = random.random()
+        if(temp < 0.25):
+            self.operator = "+"
+            self.answer = self.n1 + self.n2
+        elif(temp < 0.5):
+            self.operator = "-"
+            self.answer = self.n1 - self.n2
+        elif(temp < 0.75):
+            self.operator = "*"
+            self.answer = self.n1 * self.n2
+        else:
+            self.operator = "/"
+            self.answer = self.n1 / self.n2
+            while not(self.answer.is_integer()):
+                self.n1 = random.randrange(100)
+                self.n2 = random.randrange(100)
+                self.answer = self.n1 / self.n2
+
+    def __str__(self):
+        return f"{self.n1} {self.operator} {self.n2} = ?"
+    
+    def checkAnswer(self, check):
+        if(check == self.answer):
+            return True
+        else:
+            return False
